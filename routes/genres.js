4cc44c2382bb281559a4927cb50c221b37eb8dc2
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-05 13:35:26 +0100

    get genres db by routes

--- routes/genres.js
+++ routes/genres.js
@@ -2,10 +2,16 @@ const
     express = require("express"),
     Joi = require("joi"),
     router = express.Router(),
-    genres = [
-        { id: 1, name: "Horror" },
-        { id: 2, name: "Action" }
-    ];
+    mongoose = require("mongoose");
+
+
+
+const Genre = mongoose.model("Genre", new mongoose.Schema({
+    name: { type: String, required: true, minlength: 3, maxlength: 20, trim: true, lowercase: true }
+}));
+
+
+
 function validateGenre(genre) {
     const schema = { name: Joi.string().min(3).required() }
     return Joi.validate(genre, schema);
@@ -13,14 +19,21 @@ function validateGenre(genre) {
 
 
 
-router.get("/", (req, res) => res.send(genres));
+router.get("/", async (req, res) => {
+    try {
+        const genres = await Genre.find().sort("name");
+        res.send(genres);
+    } catch (err) { res.send("Could not get genres!") }
+});
 
 
 
-router.get("/:id", (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send("No genre on the given id");
-    res.send(genre);
+router.get("/:id", async (req, res) => {
+    try {
+        const genre = await Genre.findById(req.params.id);
+        if (!genre) return res.status(404).send("No genre on the given id");
+        res.send(genre);
+    } catch (err) { res.send(`Could not get genre on the given id:${req.params.id}`); }
 });
 
 
@@ -59,5 +72,30 @@ router.delete("/:id", (req, res) => {
 });
 
 
+
+async function createGenre(genreName) {
+    try {
+        const genre = new Genre({
+            name: genreName
+        });
+
+        const result = await genre.save();
+        return result;
+    } catch (err) { console.log(err) }
+}
+
+
+
+//createGenre("mystery")
+//    .then((result) => console.log(`Created a new genre: ${result}`))
+//    .catch((err) => console.log(`Could not create genre ${genre}!\n${err.message}`));
+
+
+
+async function getAllGenres() {
+    try {
+        return await Genre.find();
+    } catch (err) { console.log(err) }
+}
 
 module.exports = router;
