2430ab77e545b3233fbdb759ae44dc88dee3ccc3
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-05 16:04:12 +0100

    gener db delete path

--- routes/genres.js
+++ routes/genres.js
@@ -29,9 +29,9 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     try {
         const genre = await Genre.findById(req.params.id);
-        if (!genre) return res.status(404).send("No genre on the given id");
+        if (!genre) return res.status(404).send("No genre on the given id!");
         res.send(genre);
-    } catch (err) { res.send(`Could not get genre on the given id: ${req.params.id}`); }
+    } catch (err) { res.send(`Could not get genre on the given id: ${req.params.id}!`); }
 });
 
 
@@ -42,7 +42,7 @@ router.post("/", async (req, res) => {
         if (error) return res.status(400).send(error.details[0].message);
 
         res.send(await new Genre({ name: req.body.name }).save());
-    } catch (err) { res.send(`Could not post new genre ${req.body.name}`); }
+    } catch (err) { res.send(`Could not post new genre ${req.body.name}!`); }
 });
 
 
@@ -52,18 +52,14 @@ router.put("/:id", async (req, res) => {
         const genre = await Genre.findById(req.params.id);
 
         res.send(await genre.set({ name: req.body.name }).save());
-    } catch (err) { res.send(`Could not update `) }
+    } catch (err) { res.send(`Could not update ${req.params.id}!`) }
 });
 
 
 
-router.delete("/:id", (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send("No genre on the given id");
-
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-    res.send(genre);
+router.delete("/:id", async (req, res) => {
+    try { res.send(await Genre.findByIdAndRemove(req.params.id)); }
+    catch (err) { console.log(`Could not delete ${req.params.id}!`); }
 });
 
 
