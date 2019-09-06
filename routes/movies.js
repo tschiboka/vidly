0917ea4e3b8e996ab629c01c126b04dad73943b3
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-06 14:05:15 +0100

    getall getid put delete requests to movies db

--- routes/movies.js
+++ routes/movies.js
@@ -12,14 +12,20 @@ router.get("/", async (req, res) => {
 
 
 
-router.get("/:id", (req, res) => res.send("GetId"));
+router.get("/:id", async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) return res.status(404).send("No movie found on the given id " + req.params.id);
+        res.send(movie);
+    } catch (err) { console.log(err); }
+});
 
 
 
 router.post("/", async (req, res) => {
     try {
         const { error } = validate(req.body);
-        if (error) return res.status(404).send("Invalid request body!" + error.message);
+        if (error) return res.status(400).send("Invalid request body!" + error.message);
 
         res.send(await new Movie(req.body).save());
     } catch (err) { console.log(err); }
@@ -27,11 +33,28 @@ router.post("/", async (req, res) => {
 
 
 
-router.put("/:id", (req, res) => res.send("Put"));
+router.put("/:id", async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) return res.status(404).send("No movie found on the given id " + req.params.id);
+
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send("Invalid request body!" + error.message);
+
+        res.send(await movie.set(req.body).save());
+    } catch (err) { console.log(err); }
+});
 
 
 
-router.delete("/:id", (req, res) => res.send("Delete"));
+router.delete("/:id", async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) return res.status(404).send("No movie found on the given id " + req.params.id);
+
+        res.send(await movie.delete());
+    } catch (err) { console.log(err); }
+});
 
 
 
