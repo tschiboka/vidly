ce46314c7c9aa9ac1cd3663c99b4a8be0c0aa7c4
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-09-10 12:57:39 +0100

    redefine rental model

--- models/rental.js
+++ models/rental.js
@@ -4,11 +4,64 @@ const
     { customerSchema } = require("./customer"),
     { movieSchema } = require("./movie"),
     rentalSchema = mongoose.Schema({
-        customer: { type: customerSchema, required: true },
-        movies: { type: [movieSchema], required: true },
-        date: { type: Date, default: Date.now }
+        customer: {
+            type: new mongoose.Schema({
+                name: {
+                    type: String,
+                    required: true,
+                    minlength: 3,
+                    maxlength: 50
+                },
+                idGold: {
+                    type: Boolean,
+                    required: true
+                },
+                phone: {
+                    type: String,
+                    required: true,
+                    minlenght: 8,
+                    maxlength: 14
+                }
+            }),
+            required: true
+        },
+        movie: {
+            type: new mongoose.Schema({
+                title: {
+                    type: String,
+                    required: true,
+                    minlength: 5,
+                    maxlength: 255
+                },
+                dailyRentalRate: {
+                    type: Number,
+                    required: true,
+                    min: 0,
+                    max: 250
+                }
+            }),
+            required: true
+        },
+        dateOut: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+        dateReturned: Date,
+        rentalFee: {
+            type: Number,
+            min: 0
+        }
     }),
-    Rental = new mongoose.model("Rental", rentalSchema);
+    Rental = new mongoose.model("Rental", rentalSchema),
+    validateRental = (rental) => {
+        const schema = {
+            customerId: Joi.string().required(),
+            movieId: Joi.string().required()
+        }
+
+        return Joi.validate(rental, schema);
+    };
 
 
 
--- routes/rentals.js
+++ routes/rentals.js
@@ -1,7 +1,9 @@
 const
     express = require("express"),
-    Rental = require("../models/rental").Rental;
-router = express.Router();
+    Rental = require("../models/rental").Rental,
+    { Customer } = require("../models/customer"),
+    { Movie } = require("../models/movie"),
+    router = express.Router();
 
 
 
@@ -17,8 +19,27 @@ router.get("/:id", (req, res) => {
 
 
 
-router.post("/:id", (req, res) => {
-    res.send(req.body);
+router.post("/", async (req, res) => {
+    try {
+        const customer = await Customer.findById(req.body.customer);
+        if (!customer) return res.status(400).send("Could not find customer on id: " + req.body.customer);
+
+        const movies = [];
+
+        for (let i = 0; i <= req.body.movies.length - 1; i++) {
+            const movie = await Movie.findById(req.body.movies[i]);
+            if (!movie) return res.status(400).send("Could not find movie on id: " + req.body.movies[i]);
+            movies.push(movie);
+        }
+
+        console.log(customer);
+        const rental = new Rental({
+            customer: customer,
+            movies: movies
+        });
+
+        res.send(rental);
+    } catch (err) { console.log(err); }
 });
 
 
